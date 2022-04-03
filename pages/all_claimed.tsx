d98c86290e9461755a52a5860e70c6ace5b6816a
razacodespython <76620622+razacodespython@users.noreply.github.com>
2022-04-03 16:06:24 +0200

    final

---
+++ pages/all_claimed.tsx
@@ -0,0 +1,49 @@
+import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import type { NextPage } from 'next';
+import styles from '../styles/mystyles.module.css'
+import Image from 'next/image';
+import pok from '../public/pok.jpg'
+import { useNFTDrop } from '@thirdweb-dev/react';
+import { useState, useEffect } from 'react';
+import { NFTMetadata } from '@thirdweb-dev/sdk';
+
+const Claimed: NextPage = () => {
+    const address = useAddress();
+    const connectWithMetamask = useMetamask();
+    const disconnectWallet = useDisconnect();
+
+    const nftDrop = useNFTDrop("0x82747Bd4e435C9D5cF2342c8361B19910259C264")
+    const [token, setToken] = useState<NFTMetadata[]>([]);
+
+    useEffect(() => {
+        if (nftDrop) {
+            nftDrop
+                .getAllUnclaimed()
+                .then((token) => {
+                    setToken(token);
+                })
+                .catch((error) => {
+                    console.error("failed to fetch nfts", error);
+                });
+        }
+    }, [nftDrop]);
+
+    console.log(token)
+
+    return (
+        <div>
+            <div className={styles.nfts}>
+                {token.map((nft) => (
+                    <div className={styles.nfts_image}>
+                        
+                        <img src={nft.image} style={{ maxHeight: 80 }} />
+                        <p key={nft.id}>{nft.name}</p>
+                        {/* <button onClick={mintNft}>click here</button> */}
+                    </div>
+                ))}
+
+            </div>
+        </div>
+    )
+}
+export default Claimed;
